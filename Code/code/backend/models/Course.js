a5594b4033dab41cb2a12bca1af9901246fcd783
Pradeepthiturlapati <pradeepthiturlapati@gmail.com>
2025-07-23 21:20:22 +0530

    Delete Code/code directory

--- Code/code/backend/models/Course.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const courseSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: String,
-  instructor: String
-});
-
-module.exports = mongoose.model('Course', courseSchema);
